35950104999664751e06c72856e116af66681c4a
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-05-10 15:42:31 +0100

    Done with the display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -19,14 +19,17 @@ function Display() {
           <div className="name">IP Address</div>
           <div className="bold font-size">{ip.ip}</div>
         </div>
+        <hr className="line"/>
         <div className="flex gap1 items-center">
           <div className="name">Location</div>
         { ip && ip.location &&  <div className="bold font-size">{`${ip.location.city}, ${ip.location.postalCode}`}</div>}
         </div>
+        <hr className="line"></hr>
         <div className="flex gap1 items-center">
           <div className="name">Timezone</div>
           {ip && ip.location && ip.location.timezone &&  <div className="bold font-size"><span>UTC</span>{ip.location.timezone}</div>}
         </div>
+        <hr className="line"/>
         <div className="flex gap1 items-center">
           <div className="name">ISP</div>
           <div className="bold font-size">{ip.isp}</div>
