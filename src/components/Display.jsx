67dd9e6f6610c3b068485842c51aae935f30781c
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-05-10 15:09:50 +0100

    working on the desktop view

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -14,21 +14,21 @@ function Display() {
 
   return (
     <div className="display">
-      <div className="flex gap">
+      <div className="flex gap zom">
         <div className="flex gap1">
-          <div className="">IP Address</div>
+          <div className="name">IP Address</div>
           <div className="bold font-size">{ip.ip}</div>
         </div>
         <div className="flex gap1 items-center">
-          <div className="">Location</div>
+          <div className="name">Location</div>
         { ip && ip.location &&  <div className="bold font-size">{`${ip.location.city}, ${ip.location.postalCode}`}</div>}
         </div>
         <div className="flex gap1 items-center">
-          <div className="">Timezone</div>
+          <div className="name">Timezone</div>
           {ip && ip.location && ip.location.timezone &&  <div className="bold font-size"><span>UTC</span>{ip.location.timezone}</div>}
         </div>
         <div className="flex gap1 items-center">
-          <div className="">ISP</div>
+          <div className="name">ISP</div>
           <div className="bold font-size">{ip.isp}</div>
         </div>
       </div>
